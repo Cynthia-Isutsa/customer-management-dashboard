6784f0e27763bb87a025bc01ad47db6cb68417b3
cynthia-isutsa <isutsacynthia@gmail.com>
2024-04-20 00:10:43 +0300

    rendering data from the database

--- crm/Accounts/views.py
+++ crm/Accounts/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customer = Customer.objects.all()
+
+    totalCustomers = customer.count()
+
+    totalOrders = orders.count()
+
+    delivered = orders.filter(status="Delivered").count()
+
+    pending = orders.filter(status="Pending").count()
+    
+    context = {"orders": orders, "customers": customer, "totalOrders": totalOrders, 
+               "totalCustomers":totalCustomers, "delivered": delivered, "pending": pending}
+    return render(request, 'accounts/dashboard.html', context)
 
 def products(request):
-    return render(request, 'accounts/profile.html')
+    products = Product.objects.all()
+    return render(request, 'accounts/profile.html', {"products": products})
 
 def customer(request):
-    return render(request, 'accounts/customer.html')
+    return render(request, 'accounts/customer.html' )
